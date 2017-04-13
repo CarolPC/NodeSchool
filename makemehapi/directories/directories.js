861d9a484d3cff926f3341546b30c96b9a054f48
CarolPC <carolina.pascale.c@gmail.com>
2017-04-13 11:49:11 -0300

    Solution for views, directories, proxies and helping

---
+++ makemehapi/directories/directories.js
@@ -0,0 +1,27 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(Inert, (err) => {
+    if (err) throw err;
+});
+
+server.route({
+    path: '/foo/bar/baz/{filename}',
+    method: 'GET',
+    handler: {
+        directory: {
+            path: Path.join(__dirname, 'public')
+        }
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+});
